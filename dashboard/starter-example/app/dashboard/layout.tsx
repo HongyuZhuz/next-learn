13fa1c0cd0c12690ba5190fa9e77b9500d4ea984
HongyuZhuz <Hongyu.Zhu.zz@gmail.com>
2024-03-04 11:32:04 +1100

    fix the ui display bug

--- dashboard/starter-example/app/dashboard/layout.tsx
+++ dashboard/starter-example/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import SideNav from "@/app/ui/dashboard/sidenav"
 export default function Layout ({children}:{children:React.ReactNode})
 {
     return(
-        <div className="flex h-screen flex-col md:overflow-hidden">
+        <div className="flex h-screen flex-col md:overflow-hidden md:flex-row">
             <div className="w-full flex-none md:w-64">
                 <SideNav />
             </div>
